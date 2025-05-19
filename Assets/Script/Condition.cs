fb428c5bf4b5df7da20dca185295c5404a3532e9
KimJeongDae22 <rlawjdeo0895@naver.com>
2025-05-19 16:13:30 +0900

    Add : 플레이어 기본 상태 UI 추가

---
+++ Assets/Script/Condition.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Condition : MonoBehaviour
+{
+    [SerializeField] private float curValue;
+    [SerializeField] private float startValue;
+    [SerializeField] private float maxValue;
+    [SerializeField] private float passiveValue;
+    [SerializeField] private Image uiBar;
+    void Start()
+    {
+        curValue = startValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        uiBar.fillAmount = GetPercent();
+    }
+    private float GetPercent()
+        { return curValue / maxValue; }
+    public void AddValue(float value)
+    {
+        curValue = Mathf.Min(curValue + value, maxValue);
+    }
+    public void SubtractValue(float value)
+    {
+        curValue = Mathf.Max(curValue - value, 0);
+    }
+}
